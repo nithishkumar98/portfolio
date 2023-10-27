3d76a0b6e29cbe259a302bd2a33f483e26a7e9b9
Nithish Kumar M <nithishindians@gmail.com>
2023-10-27 16:33:29 +0530

    lottie-react dependencies changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react"
 import Frontend from "../lotties/Frontend.json";
 import SocialMedia from "./SocialMedia/SocialMedia";
 
